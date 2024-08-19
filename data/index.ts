f2c18c3cbdbe0cbd037e3d5ad13bf7990a3bd006
Jgranado-MSFT <84871765+JonathanGranado@users.noreply.github.com>
2024-08-18 19:12:40 -0700

    more nit fixes

--- data/index.ts
+++ data/index.ts
@@ -45,7 +45,7 @@ export const gridItems = [
     imgClassName: "",
     titleClassName: "justify-start",
     img: "./grid.svg",
-    spareImg: "/b4.svg",
+    spareImg: "./b4.svg",
   },
 
   {
@@ -55,7 +55,7 @@ export const gridItems = [
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
     img: "./b5.svg",
-    spareImg: "/grid.svg",
+    spareImg: "./grid.svg",
   },
   {
     id: 6,
@@ -74,7 +74,7 @@ export const projects = [
     id: 1,
     title: "Compiler for a syntax-similar language to Java",
     des: "A compilter that generates Java byte code from a syntax-similar language to Java. The compiler consists of a Lexer, Parser, Type Checker, and Code Generator.",
-    iconLists: ["/java-svgrepo-com.svg"],
+    iconLists: ["./java-svgrepo-com.svg"],
     img: "./p1.svg",
   },
   {
@@ -83,80 +83,20 @@ export const projects = [
       "Workout app aimed for peer-to-peer training developed as part of my CS Senior Project",
     des: "On a bike ride with a friend? With this app you can monitor each others heart rate and speed to elevate each other's performance.",
     img: "./p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    iconLists: [
+      "./next.svg",
+      "./tail.svg",
+      "./ts.svg",
+      "./stream.svg",
+      "./c.svg",
+    ],
   },
   {
     id: 3,
     title: "College major quiz",
     des: "A react based app which could help narrow down which major is best given your interests",
     img: "./p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-  },
-];
-
-export const testimonials = [
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-];
-
-export const companies = [
-  {
-    id: 1,
-    name: "cloudinary",
-    img: "./cloud.svg",
-    nameImg: "/cloudName.svg",
-  },
-  {
-    id: 2,
-    name: "appwrite",
-    img: "./app.svg",
-    nameImg: "/appName.svg",
-  },
-  {
-    id: 3,
-    name: "HOSTINGER",
-    img: "./host.svg",
-    nameImg: "/hostName.svg",
-  },
-  {
-    id: 4,
-    name: "stream",
-    img: "./s.svg",
-    nameImg: "/streamName.svg",
-  },
-  {
-    id: 5,
-    name: "docker.",
-    img: "./dock.svg",
-    nameImg: "/dockerName.svg",
+    iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./c.svg"],
   },
 ];
 
@@ -166,21 +106,21 @@ export const workExperience = [
     title: "Software Engineer - Microsoft",
     desc: "Under the load balancing team of Substrate Core, helping to maintain and improve distributed systems.",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "./exp1.svg",
   },
   {
     id: 2,
     title: "Software Engineer Intern - Microsoft",
     desc: "Learned how to create Window applications with C++ Win32 and developed a new touch screen animation for my team. ",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    thumbnail: "./exp2.svg",
   },
   {
     id: 3,
     title: "Software Engineer Co-op - GE Appliances",
     desc: "Worked with the Android team to help create a mobile app to ease testing of appliances, being able to connect to them via the app. Learned how to develop in Kotlin and how to use C for embedded sytems. ",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    thumbnail: "./exp3.svg",
   },
 ];
 
