9f33c84a9ec79e853380aeac53de16773b120c51
Jgranado-MSFT <84871765+JonathanGranado@users.noreply.github.com>
2026-06-14 13:24:40 -0700

    update portfolio

---
+++ components/CodingProblem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CodingProblem = () => {
+  return (
+    <section id="coding">
+      <div className=" h-[90vh] w-full pb-10">
+        <h1 className="heading pb-5 mt-10">
+          Interested in <span className="text-purple">a challenge?</span>
+        </h1>
+        <div className="bg-pink-400 h-full"></div>
+      </div>
+    </section>
+  );
+};
+
+export default CodingProblem;
